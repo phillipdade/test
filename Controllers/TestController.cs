d1362e8736c328fd62358e1dfbb52dd791508834
phillipdade <phillip.dade@me.com>
2023-03-02 10:44:31 +0000

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Form(UserForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Output", model);
+            }
+
+            return View(model);
+        }
+    }
+}
+
---
+++ ViewModels/UserForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace test.ViewModels
+{
+	public class UserForm
+	{
+		[Display(Name="First Name")]
+		public string? FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+
+        [Display(Name = "Random Text Field")]
+        public string? TextField { get; set; }
+
+    }
+}
+
